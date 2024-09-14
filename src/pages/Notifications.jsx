b72d13261189cbd5b4505aa3f4f12b60c04691be
Ambika Prasad <akashboard2021@gmail.com>
2024-09-15 04:46:21 +0530

    notification spinner bug

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -11,28 +11,28 @@ import {
 } from "../services/functions/notificationFn";
 import SpinnerSm from "../ui/SpinnerSm";
 import toast from "react-hot-toast";
-const notifiData = [
-  {
-    id: 1,
-    time: "12:00 AM",
-    text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
-  },
-  {
-    id: 2,
-    time: "12:00 AM",
-    text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
-  },
-  {
-    id: 3,
-    time: "12:00 AM",
-    text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
-  },
-  {
-    id: 4,
-    time: "12:00 AM",
-    text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
-  },
-];
+// const notifiData = [
+//   {
+//     id: 1,
+//     time: "12:00 AM",
+//     text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
+//   },
+//   {
+//     id: 2,
+//     time: "12:00 AM",
+//     text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
+//   },
+//   {
+//     id: 3,
+//     time: "12:00 AM",
+//     text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
+//   },
+//   {
+//     id: 4,
+//     time: "12:00 AM",
+//     text: "To increase the distance between each dash in a dashed border, you can use the border-style property with the border-width and border-color properties alongside the border-image property for more precise control.",
+//   },
+// ];
 
 const StyledDiv = styled.div`
   display: flex;
@@ -90,27 +90,39 @@ function Notifications() {
     queryFn: getNotifications,
   });
 
+  const [processingNotification, setProcessingNotification] = useState(null);
+
   const { isLoading: markLoading, mutate: markMutate } = useMutation({
     mutationKey: ["notification"],
     mutationFn: markNotificationSeen,
+    onMutate: (notificationId) => {
+      setProcessingNotification(notificationId);
+    },
     onSuccess: () => {
       toast.success("Marked as seen");
       queryClient.invalidateQueries(["notification"]);
+      setProcessingNotification(null);
     },
     onError: (err) => {
       toast.error(err.message);
+      setProcessingNotification(null);
     },
   });
 
   const { isLoading: loadingDelete, mutate: deleteMutate } = useMutation({
     mutationKey: ["notification"],
     mutationFn: deleteNotification,
+    onMutate: (notificationId) => {
+      setProcessingNotification(notificationId);
+    },
     onSuccess: () => {
       toast.success("Notification deleted successfully.");
       queryClient.invalidateQueries(["notification"]);
+      setProcessingNotification(null);
     },
     onError: (err) => {
       toast.error(err.message);
+      setProcessingNotification(null);
     },
   });
 
@@ -136,7 +148,7 @@ function Notifications() {
         <NoNotifications>There are no notifications for now.</NoNotifications>
       )}
       {notifications.map((el) => (
-        <Container key={el.id}>
+        <Container key={el._id}>
           <BsBellFill />
           <Content>
             <time>{getTime(el.createdAt)}</time>
@@ -147,7 +159,7 @@ function Notifications() {
                 size="small"
                 onClick={() => deleteMutate(el.id)}
               >
-                {loadingDelete ? <SpinnerSm /> : "Delete"}
+                {processingNotification === el._id ? <SpinnerSm /> : "Delete"}
               </Button>
               <span></span>
               <Button
@@ -155,8 +167,13 @@ function Notifications() {
                 size="small"
                 onClick={() => markMutate(el.id)}
               >
-                {markLoading && <SpinnerSm />}
-                {!markLoading && (el.seen ? "Already read" : "Mark as read")}
+                {processingNotification === el._id ? (
+                  <SpinnerSm />
+                ) : el.seen ? (
+                  "Already read"
+                ) : (
+                  "Mark as read"
+                )}
               </Button>
             </div>
           </Content>
